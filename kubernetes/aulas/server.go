fd909e5bba38ef80fecc1d233017f49a0a26f2e5
Julio Saldanha <juliosaldanha71@gmail.com>
2023-01-08 15:28:08 -0300

    feat: config map and secrets

--- kubernetes/aulas/server.go
+++ kubernetes/aulas/server.go
@@ -1,12 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/secret", Secret)
 	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello Full Cycle!!!</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s", name, age)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
 }
